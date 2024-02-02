d7bc50cafbb5b57a45affd1b293768c9bc685c97
Yuriy Bodolanov <magikonermad@yandex.ru>
2024-02-02 16:07:38 +0300

    Исправление роутера

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.scss';
 import App from './components/App/App';
 import { Provider } from 'react-redux';
@@ -9,10 +9,10 @@ import { store } from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
